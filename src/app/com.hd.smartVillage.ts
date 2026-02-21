7316fdf8bc4e4af0aedea0c066a69bd61773fa19
AliusCode <2039917389@qq.com>
2026-02-22 00:06:21 +0800

    perf: 权限规则删除

--- src/app/com.hd.smartVillage.ts
+++ src/app/com.hd.smartVillage.ts
@@ -6,28 +6,6 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '权限提示-通知权限',
-      fastQuery: true,
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: [
-        {
-          activityIds: [
-            '.modules.main.MainActivity',
-            '.modules.h5service.H5WebActivity',
-          ],
-          matches:
-            'View[id="com.hd.smartVillage:id/notificationSettingCloseBtn"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/13223669',
-            'https://i.gkd.li/i/13293000',
-          ],
-        },
-      ],
-    },
-    {
-      key: 2,
       name: '更新提示',
       fastQuery: true,
       matchTime: 10000,
