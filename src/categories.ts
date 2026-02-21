6404a730f130af655b26404556f4a19215f20f42
AliusCode <2039917389@qq.com>
2026-02-21 22:57:29 +0800

    perf: 取消权限提示规则

--- src/categories.ts
+++ src/categories.ts
@@ -28,39 +28,33 @@ export default defineGkdCategories([
     enable: false,
   },
   {
-    // 各种权限请求，如定位权限、通知权限等
-    key: 5,
-    name: '权限提示',
-    enable: false,
-  },
-  {
     // 不影响使用的广告, 所有的广告卡片及悬浮广告按钮
-    key: 6,
+    key: 5,
     name: '局部广告',
     enable: false,
   },
   {
     // 不关闭广告就无法进行其他操作的广告弹窗, 它阻碍用户操作
-    key: 7,
+    key: 6,
     name: '全屏广告',
     enable: false,
   },
   {
     // 如微信朋友圈广告/酷安帖子广告/贴吧帖子广告
     // 需要分阶段多步操作才能关闭, 会造成屏幕闪烁, 影响用户体验
-    key: 8,
+    key: 7,
     name: '分段广告',
     enable: false,
   },
   {
     // 非广告, 点击某些功能按钮
-    key: 9,
+    key: 8,
     name: '功能类',
     enable: false,
   },
   {
     // 分类不明确
-    key: 10,
+    key: 9,
     name: '其他',
     enable: false,
   },
