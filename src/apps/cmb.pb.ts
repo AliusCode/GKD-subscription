24ce6f4a7c2ec60d8a2ed76ff7dcca713236d40a
AliusCode <2039917389@qq.com>
2026-02-21 23:36:43 +0800

    perf: 权限规则删除

--- src/apps/cmb.pb.ts
+++ src/apps/cmb.pb.ts
@@ -39,27 +39,5 @@ export default defineGkdApp({
         },
       ],
     },
-    {
-      key: 3,
-      name: '权限提示-定位权限',
-      fastQuery: true,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: [
-        {
-          activityIds: [
-            '.app.h5container.webviewcontainer.PBWebContainerActivity',
-            '.app.search.PBSearchWebActivity',
-            'com.pb.marketing.PopupWindowActivity',
-          ],
-          matches: '@[desc="关闭"] - * > [text*="开启"][text*="定位"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/13248893',
-            'https://i.gkd.li/i/13897345',
-            'https://i.gkd.li/i/14208717',
-          ],
-        },
-      ],
-    },
   ],
 });
