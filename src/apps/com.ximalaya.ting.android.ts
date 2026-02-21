810ccb57cbf7720429e89df50d1aa44a15eb35db
AliusCode <2039917389@qq.com>
2026-02-21 23:26:46 +0800

    perf: 权限规则删除

--- src/apps/com.ximalaya.ting.android.ts
+++ src/apps/com.ximalaya.ting.android.ts
@@ -113,29 +113,6 @@ export default defineGkdApp({
     },
     {
       key: 6,
-      name: '权限提示-通知权限',
-      desc: '取消推送通知',
-      fastQuery: true,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: [
-        {
-          activityIds: '.host.activity.MainActivity',
-          matches: [
-            '[text*="通知权限"][visibleToUser=true]',
-            '[text="取消" || text="暂不开启"][visibleToUser=true]',
-          ],
-          exampleUrls: 'https://e.gkd.li/ab40c096-d024-4b7c-9c6f-245beafd373a',
-          snapshotUrls: [
-            'https://i.gkd.li/i/13389145',
-            'https://i.gkd.li/i/18391977',
-            'https://i.gkd.li/i/20499323',
-          ],
-        },
-      ],
-    },
-    {
-      key: 7,
       name: '全屏广告-免流提示',
       desc: '关闭[开免流送会员]弹窗',
       rules: [
