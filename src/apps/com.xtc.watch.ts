8fe18ffd92a6c416db6c8bfe3c6f74f311eea705
AliusCode <2039917389@qq.com>
2026-02-21 23:25:07 +0800

    perf: 权限规则删除

--- src/apps/com.xtc.watch.ts
+++ src/apps/com.xtc.watch.ts
@@ -23,30 +23,5 @@ export default defineGkdApp({
         },
       ],
     },
-    {
-      key: 2,
-      name: '权限提示-通知权限',
-      fastQuery: true,
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: [
-        {
-          key: 0,
-          activityIds: 'com.xtc.widget.phone.popup.activity.CustomActivity13',
-          matches: [
-            '[text*="开启消息通知"]',
-            '[id="com.xtc.watch:id/iv_close"]',
-          ],
-          snapshotUrls: 'https://i.gkd.li/i/13063274',
-        },
-        {
-          key: 1,
-          activityIds: '.view.home.activity.XtcHomeActivity',
-          matches: '[id="com.xtc.watch:id/iv_notify_permission_close"]',
-          snapshotUrls: 'https://i.gkd.li/i/13059965',
-        },
-      ],
-    },
   ],
 });
